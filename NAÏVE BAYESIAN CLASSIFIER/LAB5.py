6483d8d36a7b5075e8844a7690537f0aa0c1c0cf
Deva22AIA29 <165248141+Deva22AIA29@users.noreply.github.com>
2024-05-28 09:47:34 +0530

    Update LAB5.py

--- NAÏVE BAYESIAN CLASSIFIER/LAB5.py
+++ NAÏVE BAYESIAN CLASSIFIER/LAB5.py
@@ -1,44 +1,56 @@
-# import necessary libarities
 import pandas as pd
 from sklearn import tree
 from sklearn.preprocessing import LabelEncoder
 from sklearn.naive_bayes import GaussianNB
-
-# load data from CSV
-data = pd.read_csv('tennisdata.csv')
-print("THe first 5 values of data is :\n",data.head())
-
-# obtain Train data and Train output
-X = data.iloc[:,:-1]
-print("\nThe First 5 values of train data is\n",X.head())
-
-y = data.iloc[:,-1]
-print("\nThe first 5 values of Train output is\n",y.head())
-
-# Convert then in numbers 
-le_outlook = LabelEncoder()
-X.Outlook = le_outlook.fit_transform(X.Outlook)
-
-le_Temperature = LabelEncoder()
-X.Temperature = le_Temperature.fit_transform(X.Temperature)
-
-le_Humidity = LabelEncoder()
-X.Humidity = le_Humidity.fit_transform(X.Humidity)
-
-le_Windy = LabelEncoder()
-X.Windy = le_Windy.fit_transform(X.Windy)
-
-print("\nNow the Train data is :\n",X.head())
-
-le_PlayTennis = LabelEncoder()
-y = le_PlayTennis.fit_transform(y)
-print("\nNow the Train output is\n",y)
-
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.20)
-
-classifier = GaussianNB()
-classifier.fit(X_train,y_train)
-
 from sklearn.metrics import accuracy_score
-print("Accuracy is:",accuracy_score(classifier.predict(X_test),y_test))
+import streamlit as st
+
+# Load data from CSV
+@st.cache
+def load_data():
+    return pd.read_csv('tennisdata.csv')
+
+def main():
+    st.title("Tennis Play Prediction")
+
+    data = load_data()
+    st.subheader("Preview of the Data")
+    st.write(data.head())
+
+    # Obtain Train data and Train output
+    X = data.iloc[:,:-1]
+    y = data.iloc[:,-1]
+
+    # Convert them to numbers
+    le_outlook = LabelEncoder()
+    X.Outlook = le_outlook.fit_transform(X.Outlook)
+
+    le_Temperature = LabelEncoder()
+    X.Temperature = le_Temperature.fit_transform(X.Temperature)
+
+    le_Humidity = LabelEncoder()
+    X.Humidity = le_Humidity.fit_transform(X.Humidity)
+
+    le_Windy = LabelEncoder()
+    X.Windy = le_Windy.fit_transform(X.Windy)
+
+    le_PlayTennis = LabelEncoder()
+    y = le_PlayTennis.fit_transform(y)
+
+    # Train-test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+
+    # Train the model
+    classifier = GaussianNB()
+    classifier.fit(X_train, y_train)
+
+    # Prediction and Evaluation
+    y_pred = classifier.predict(X_test)
+    accuracy = accuracy_score(y_pred, y_test)
+    
+    st.subheader("Model Evaluation")
+    st.write("Accuracy:", accuracy)
+
+if __name__ == "__main__":
+    main()
